0e05d98fc8c0ccd4c41892d444b59069b466b4f5
KruzhokProg <Aa1@3$5^7>
2021-10-11 17:59:57 +0300

    меню + json

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ def bot_message(message):
                 json_dump = json.dumps('[{"id":88055,"answer":"10","question":"Bo Derek probably knows diamonds rate this on the Mohs scale, which measures hardness","value":200,"airdate":"2009-07-06T12:00:00.000Z","created_at":"2014-02-14T01:53:42.801Z","updated_at":"2014-02-14T01:53:42.801Z","category_id":3953,"game_id":null,"invalid_count":null,"category":{"id":3953,"title":"diamonds are forever","created_at":"2014-02-11T23:05:51.795Z","updated_at":"2014-02-11T23:05:51.795Z","clues_count":15}}]')
                 # json_res = json.loads('https://jservice.io/api/random?count=1')
                 json_res = json.loads(json_dump)
-                bot.send_message(message.from_user.id, str(json_res[0]))
+                bot.send_message(message.from_user.id, json_res[0])
             except Exception as ex:
                 print(ex)
         # if message.text == 'привет!':
