27943d501f3448ee41c6c122962eca3982f42194
KruzhokProg <Aa1@3$5^7>
2021-10-11 17:43:34 +0300

    меню + json

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import types
 import telebot
 from flask import Flask, request
 import json
@@ -17,6 +18,13 @@ def start(message):
         pic = open('welcome.tgs', 'rb')
         bot.send_sticker(message.chat.id, pic)
         bot.send_message(message.from_user.id, "✋ Добро пожаловать!")
+
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        item1 = types.KeyboardButton('Викторина')
+        item2 = types.KeyboardButton('Числа')
+        markup.add(item1, item2)
+        bot.send_message(message.chat.id, f'Привет! <b> {message.from_user} </b>'
+                         , parse_mode='html', reply_markup=markup)
     except Exception as ex:
         print(ex)
 
@@ -24,10 +32,13 @@ def start(message):
 @bot.message_handler(content_types=['text'])
 def bot_message(message):
     if message.chat.type == 'private':
-        if message.text == 'привет!':
-            bot.send_message(message.from_user.id, 'Здаров!')
-        else:
-            bot.send_message(message.from_user.id, "давайте знакомится?!")
+        if message.text == 'Викторина':
+            json_res = json.loads('https://jservice.io/api/random?count=1')
+            bot.send_message(message.from_user.id, json_res["question"])
+        # if message.text == 'привет!':
+        #     bot.send_message(message.from_user.id, 'Здаров!')
+        # else:
+        #     bot.send_message(message.from_user.id, "давайте знакомится?!")
 
 
 @server.route('/' + TOKEN, methods=['POST'])
