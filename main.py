b3855af326883c0b3acdc0c9467bd72259c8a1c4
KruzhokProg <Aa1@3$5^7>
2021-10-11 17:14:50 +0300

    Приветствие + картинка

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 
 import telebot
 from flask import Flask, request
+import json
 
 TOKEN = "2003680813:AAHB85TfLFidKVKHZFPEaJQsxa2Nzp_we8Y"
 
@@ -9,19 +10,25 @@ APP_URL = f"https://test-itdop-bot2021.herokuapp.com/{TOKEN}"
 
 bot = telebot.TeleBot(TOKEN)
 server = Flask(__name__)
+
+
 # вапвп
 
-# @bot.message_handler(commands=['start'])
-# def start(message):
-#     bot.send_message("Добро пожаловать!")
+@bot.message_handler(commands=['start'])
+def start(message):
+    pic = open('welcome.jpeg', 'rb')
+    bot.send_photo(message.chat.id, pic)
+    bot.send_message(message.from_user.id, "✋ Добро пожаловать!")
+
 
 @bot.message_handler(content_types=['text'])
 def bot_message(message):
     if message.chat.type == 'private':
         if message.text == 'привет!':
-            bot.send_message(message.from_user.id,'Здаров!')
+            bot.send_message(message.from_user.id, 'Здаров!')
         else:
             bot.send_message(message.from_user.id, "давайте знакомится?!")
+
 
 @server.route('/' + TOKEN, methods=['POST'])
 def get_message():
