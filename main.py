2f3ca01176bb0126c38c56d82ea63a70167bd23d
KruzhokProg <Aa1@3$5^7>
2021-10-16 19:36:53 +0300

    numbers

--- main.py
+++ main.py
@@ -51,7 +51,7 @@ def bot_message(message):
         elif message.text == 'Числа':
             try:
                 response = requests.get('http://numbersapi.com/11/trivia')
-                data = str(response.json())
+                data = str(response)
                 bot.send_message(message.from_user.id, data)
             except Exception as ex:
                 print(ex)
