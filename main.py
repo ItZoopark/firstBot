090fe6924cee4f34fdce9b44243d027f03d74fc3
KruzhokProg <Aa1@3$5^7>
2021-12-10 14:25:41 +0300

    работа с БД

--- main.py
+++ main.py
@@ -150,8 +150,8 @@ def createStudent(message):
     print(num)
     print(letter)
     response = requests.get(f'https://school-estimate-django-rest.herokuapp.com/api/v1/?num={num}&letter={letter}')
-    response_json_str = str(response.json()).replace("\'", "\"").replace('None', 'null')
-    print(response_json_str)
+    # response_json_str = str(response.json()).replace("\'", "\"").replace('None', 'null')
+    print(response)
     # json_res = json.loads(response_json_str)
     # grade_id = json_res["id"]
     # response = requests.post('https://school-estimate-django-rest.herokuapp.com/api/v1/student/',
